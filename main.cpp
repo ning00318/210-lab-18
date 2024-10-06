710b723774f96c78eda3423d5e3163d665596906
NINA <ninatsao@ningningning.lan>
2024-10-06 03:03:23 -0700

    13th commit

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ struct Node {
     Node *next;
 };
 
+// prototypes
 Node *addNodesToHead(Node*, float, string);
 Node *addNodesToTail(Node*, float, string);
 void outputReviews(Node*, int);
@@ -50,7 +51,7 @@ int main() {
         else
             cout << "Please enter choices correctly (option 1 or 2) and try again" << endl << endl;
     
-    } while (choice != 1 && choice != 2);
+    } while (choice != 1 && choice != 2);   // repeat until choice is not both equal to 1 and 2
 
     outputReviews(head, count);
 
@@ -78,6 +79,6 @@ Node *addNodesToTail(Node *head, float rating, string comments) {
 void outputReviews(Node *head, int count) {
 
     cout << "Outputting all reviews:" << endl;
-    cout << "\t> Review #" << count << ": ";
-    cout << "\t> Average: ";
+    cout << "\t> Review #" << count << ": " << endl;
+    cout << "\t> Average: " << endl;
 }
