dffd9bbf23256bbc3d898b2ddf087a1aa002be48
NINA <ninatsao@ningningning.lan>
2024-10-06 01:14:49 -0700

    3nd commit

--- main.cpp
+++ main.cpp
@@ -12,15 +12,22 @@ struct Node {
 int main() {
     int choice;
 
-    cout << "Which linked list method should we use?" << endl;
-    cout << "\t[1] New nodes are added at the head of the linked list" << endl;
-    cout << "\t[2] New nodes are added at the tail of the linked list" << endl;
-    cout << "\tChoice: ";
-    cin >> choice;
+    do {
+        cout << "Which linked list method should we use?" << endl;
+        cout << "\t[1] New nodes are added at the head of the linked list" << endl;
+        cout << "\t[2] New nodes are added at the tail of the linked list" << endl;
+        cout << "\tChoice: ";
+        cin >> choice;
 
-    while (choice == 1 || choice == 2) {
-        
-    }
+        if (choice == 1) {
+
+        }
+        else if (choice == 2) {
+
+        }
+        else
+            cout << "Please enter choices correctly (option 1 or 2) and try again" << endl;
+    } while (choice != 1 || choice != 2);
 
     return 0;
 }
