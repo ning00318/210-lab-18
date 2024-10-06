b5581dce2dc41b1fd0d16029ed0ea718d837af84
NINA <ninatsao@ningningning.lan>
2024-10-06 01:36:05 -0700

    5th commit

--- main.cpp
+++ main.cpp
@@ -11,8 +11,10 @@ void addNodesToHead(Node *);
 void addNodesToTail(Node *);
 
 int main() {
-    int choice;
     Node *head = nullptr;
+    int choice;
+    int rating;
+    string comments;
 
     do {
         cout << "Which linked list method should we use?" << endl;
@@ -20,16 +22,33 @@ int main() {
         cout << "\t[2] New nodes are added at the tail of the linked list" << endl;
         cout << "\tChoice: ";
         cin >> choice;
+        cin.ignore();
 
-        if (choice == 1) {
-            addNodesToHead(head);
-        }
-        else if (choice == 2) {
-            addNodesToTail(head);
+        if (choice == 1 || choice == 2) {
+            cout << "Enter review rating 0-5: ";
+            cin >> rating;
+            cin.ignore();
+
+            cout << "\nEnter review comments: ";
+            getline(cin, comments);
+
+            if (choice == 1)
+                addNodesToHead(head);
+            else
+                addNodesToTail(head);            
         }
         else
             cout << "Please enter choices correctly (option 1 or 2) and try again" << endl;
+    
     } while (choice != 1 || choice != 2);
 
     return 0;
+}
+
+void addNodesToHead(Node *head) {
+    
+}
+
+void addNodesToTail(Node *head) {
+    
 }
